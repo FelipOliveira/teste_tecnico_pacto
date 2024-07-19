bc030bab7d6bcecbc246e07e4395a7acb4d492a3
FelipOliveira <sem.ideias.na.hora@gmail.com>
2024-07-19 15:44:31 -0300

    first commit

---
+++ src/main/java/com/br/foliveira/recrutamento_interno/RecrutamentoInternoApplication.java
@@ -0,0 +1,13 @@
+package com.br.foliveira.recrutamento_interno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutamentoInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutamentoInternoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/foliveira/recrutamento_interno/RecrutamentoInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.foliveira.recrutamento_interno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutamentoInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
